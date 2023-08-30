634e6a1c02c0ef02f0eabd6ebccf4aec35cb8985
quangvann <ngovanquang103@gmail.com>
2023-08-30 21:15:35 +0700

    be_order

--- BE/app.js
+++ BE/app.js
@@ -1,1 +1,24 @@
-console.log("Hello!");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import orderRoutes from './routes/orderRoutes.js';
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use("/api", orderRoutes);
+
+const port = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/DATN")
+  .then(() => console.log("Connect to DB successfully"));
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
