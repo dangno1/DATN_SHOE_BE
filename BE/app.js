86d67ffe7d0defcee49f0a3f2b99ebef2f241c9a
truongduy03 <truongduy97a@gmail.com>
2023-09-08 08:53:42 +0700

    KAN-5-User

--- BE/app.js
+++ BE/app.js
@@ -1,1 +1,26 @@
-console.log("Hello!");
+import express from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+import routerAuth from "./src/routes/auth.js";
+import routerUser from "./src/routes/user.js";
+
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/api", routerAuth);
+app.use("/api", routerUser);
+
+const port = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/DATN")
+  .then(() => console.log("Connect to DB successfully"));
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
