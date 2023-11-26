4f5c61b7fddb42c4ef9dfd24e447043a17321de9
truongduy03 <truongduy97a@gmail.com>
2023-11-26 21:15:42 +0700

    comment admin

--- BE/src/routers/commentRouter.js
+++ BE/src/routers/commentRouter.js
@@ -4,6 +4,7 @@ import {getComments} from '../controllers/Comment/getComment'
 import { deleteComment } from '../controllers/Comment/deleteComment';
 import { getcommentID } from '../controllers/Comment/getcommentID';
 import { checkPermission } from '../middlewares/checkPermision';
+import { deleteCommentAdmin, deleteCommentsAdmin } from '../controllers/Comment/deleteCommentAdmin';
 const commentRouter = express.Router();
 
 
@@ -11,5 +12,7 @@ commentRouter.post('/comments', createComment);
 commentRouter.get('/comments', getComments );
 commentRouter.get('/comments/:id', getcommentID );
 commentRouter.delete('/comments/:id', deleteComment);
+commentRouter.delete('/commentAdmin/:id', deleteCommentAdmin);
+commentRouter.delete('/commentsAdmin', deleteCommentsAdmin);
 
 export default commentRouter;
