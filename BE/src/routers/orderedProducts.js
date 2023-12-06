fb584f8fcc1aaab4e3222b6d0631469ae8b99650
dangnd <dangne23102003@gmail.com>
2023-12-06 22:41:13 +0700

    sendEmailtoUser

--- BE/src/routers/orderedProducts.js
+++ BE/src/routers/orderedProducts.js
@@ -4,6 +4,7 @@ import getOredrs from "../controllers/orderedProduct/getAllOrderProduct.js";
 import updateStatus from "../controllers/orderedProduct/updateOrder.js";
 import checkout from "../controllers/orderedProduct/checkout.js";
 import updateStatusAdmin from "../controllers/orderedProduct/updateOrderAdmin.js";
+import sendEmailConfirmation from "../controllers/orderedProduct/sendEmailConfirmation.js";
 const routerOderProduct = express.Router();
 
 routerOderProduct.post("/addOderProduct", addOrderedProduct);
@@ -11,4 +12,5 @@ routerOderProduct.get("/orderProducts", getOredrs)
 routerOderProduct.patch("/orderProducts/:id", updateStatus);
 routerOderProduct.patch("/orderProductUpdate/:id", updateStatusAdmin)
 routerOderProduct.post('/checkout', checkout);
+routerOderProduct.post("/sendEmail", sendEmailConfirmation);
 export default routerOderProduct;
