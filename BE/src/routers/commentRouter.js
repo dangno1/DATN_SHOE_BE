33fbed490a0d2dae841a1445bfffb6c7b7c85a9d
dangnd <dangne23102003@gmail.com>
2023-12-06 22:52:15 +0700

    update delete comment

--- BE/src/routers/commentRouter.js
+++ BE/src/routers/commentRouter.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import { createComment } from '../controllers/Comment/createComment';
-import {getComments} from '../controllers/Comment/getComment'
-import { getcommentID } from '../controllers/Comment/getcommentID';
-import { checkPermission } from '../middlewares/checkPermision';
-import { deleteCommentAdmin, deleteCommentsAdmin } from '../controllers/Comment/deleteCommentAdmin';
-// import { deleteComment } from '../controllers/comment/deleteComment.js';
+import express from "express";
+import { createComment } from "../controllers/Comment/createComment";
+import { getComments } from "../controllers/Comment/getComment";
+import { getcommentID } from "../controllers/Comment/getcommentID";
+import {
+  deleteCommentAdmin,
+  deleteCommentsAdmin,
+} from "../controllers/Comment/deleteCommentAdmin";
+
+import { deleteComment } from "../controllers/Comment/deleteComment.js";
+
 const commentRouter = express.Router();
 
-
-commentRouter.post('/comments', createComment);
-commentRouter.get('/comments', getComments );
-commentRouter.get('/comments/:id', getcommentID );
-commentRouter.delete('/commentAdmin/:id', deleteCommentAdmin);
-commentRouter.delete('/commentsAdmin', deleteCommentsAdmin);
-// commentRouter.delete('/comments/:id', deleteComment);
+commentRouter.post("/comments", createComment);
+commentRouter.get("/comments", getComments);
+commentRouter.get("/comments/:id", getcommentID);
+commentRouter.delete("/commentAdmin/:id", deleteCommentAdmin);
+commentRouter.delete("/commentsAdmin", deleteCommentsAdmin);
+commentRouter.delete("/comments/:id", deleteComment);
 
 export default commentRouter;
